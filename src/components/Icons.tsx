8148718804cfc160886b43af6b3f0b0658a44378
codeEnthusiast21 <aviralagarwal190006@gmail.com>
2025-06-05 01:24:47 +0530

    added styling

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -4,14 +4,24 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 
 
 type IconsProps = PropsWithChildren<{
-    name:string,
+    name: string,
 
 
 }>
-const Icons = ({name }) => {
-  return (
-    <div>Icons</div>
-  )
+const Icons = ({ name }: IconsProps) => {
+    switch (name) {
+        case 'circle':
+            return <Icon name='circle-thin' size={38}
+                color="#F7CD2E" />
+            break;
+        case 'cross':
+            return <Icon name='times' size={38}
+                color="#38CC77" />
+            break;
+        default:
+              return <Icon name='pencil' size={38}
+                color="#0D0D0D" />
+    }
 }
 
 export default Icons
